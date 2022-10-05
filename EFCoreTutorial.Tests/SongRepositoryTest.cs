c445b11587ea24835ab7e22654bc1f59d681c210
jkresak101 <jkresak101@gmail.com>
2022-10-05 08:37:43 -0700

    update

--- EFCoreTutorial.Tests/SongRepositoryTest.cs
+++ EFCoreTutorial.Tests/SongRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EFCoreTutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,74 @@ using System.Threading.Tasks;
 
 namespace EFCoreTutorial.Tests
 {
-    public class SongRepositoryTests
+    public class SongRepositoryTests : IDisposable
     {
-        [Fact]
-        public void TODO()
+        private MusicContext db;
+        private SongRepository underTest;
+
+        public SongRepositoryTests()
         {
-            Assert.True(true);
+            db = new MusicContext();
+            db.Database.BeginTransaction();
+
+            underTest = new SongRepository(db);
         }
+        [Fact]
+        public void Count_Starts_At_Zero()
+        {
+            var count = underTest.Count();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void Create_Increases_Count()
+        {
+            underTest.Create(new Song() { Title = "Foo" });
+
+            var count = underTest.Count();
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void GetById_Returns_Created_Item()
+        {
+            var expectedSong = new Song() { Title = "Baby Shark" };
+            underTest.Create(expectedSong);
+
+            var result = underTest.GetById(expectedSong.Id);  // The Id was set by EF when we call Create above.
+
+            Assert.Equal(expectedSong.Title, result.Title);
+        }
+
+        [Fact]
+        public void Delete_Reduces_Count()
+        {
+            var song = new Song() { Title = "Baby Shark" };
+            underTest.Create(song);
+
+            underTest.Delete(song);
+            var count = underTest.Count();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void GetAll_Returns_All()
+        {
+            underTest.Create(new Song() { Title = "Baby Shark" });
+            underTest.Create(new Song() { Title = "Never gonna give you up" });
+
+            var all = underTest.GetAll();
+
+            Assert.Equal(2, all.Count());
+        }
+        public void Dispose()
+        {
+            db.Database.RollbackTransaction();
+        }
+
+        // Save or Update?
     }
-}
+    }
+
--- EFCoreTutorial/SongRepository.cs
+++ EFCoreTutorial/SongRepository.cs
@@ -7,23 +7,49 @@ using System.Threading.Tasks;
 
 namespace EFCoreTutorial 
 {
-    public class SongRepository
+    public class SongRepository : Repository<Song>
     {
         private MusicContext db;
 
-        public SongRepository(MusicContext db)
+        public SongRepository(MusicContext context) : base(context)
         {
             this.db = db;
         }
 
         public int Count()
         {
-            return 1;
+            return db.Songs.Count();
         }
+
         public void Create(Song song)
         {
-            db.Songs.Add(song);
+            db.Songs?.Add(song);
             db.SaveChanges();
+        }
+
+        public Song GetById(int id)
+        {
+            return db.Songs.Single(s => s.Id == id);
+        }
+
+        public void Delete(Song song)
+        {
+            db.Songs?.Remove(song);
+            db.SaveChanges();
+        }
+
+        public void Save()
+        {
+            // Save will update all song instances that have been modified in the DB.
+            // Theres no great way to test this, and really we would be testing Microsoft's
+            // code and not ours.
+            db.SaveChanges();
+        }
+
+        public IEnumerable<Song> GetAll()
+        {
+            return db.Songs.ToList();
         }
     }
 }
+
